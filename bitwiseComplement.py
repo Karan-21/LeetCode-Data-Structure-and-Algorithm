ab4743718b7dcf985dc8471430a070accf938211
Karan <64781870+Karan-21@users.noreply.github.com>
2026-03-11 23:13:15 +1030

    Create bitwiseComplement.py

---
+++ bitwiseComplement.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def bitwiseComplement(self, n):
+		return ((2 << int(math.log(max(n, 1), 2))) - 1) - n
+        # return ((2 << int(math.log(max(n, 1), 2))) - 1) ^ n  # also work
+        # return ((2 << int(math.log(max(n, 1), 2))) - 1) & (~n)  # also work
