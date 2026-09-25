e7186656be8cb50aac9063bc72ff76c606c2db26
Karan <64781870+Karan-21@users.noreply.github.com>
2026-09-25 14:20:57 +0930

    Create braceExpansionII.py

---
+++ braceExpansionII.py
@@ -0,0 +1,45 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        self.s = expression
+        self.i = 0
+
+        def concat(a, b):
+            return {x + y for x in a for y in b}
+
+        def parse_expr():
+            # Parse concatenated expressions until ',' or '}'
+            res = {""}
+
+            while self.i < len(self.s) and self.s[self.i] not in "},":
+                if self.s[self.i] == "{":
+                    cur = parse_brace()
+                else:
+                    cur = {self.s[self.i]}
+                    self.i += 1
+
+                res = concat(res, cur)
+
+            return res
+
+        def parse_brace():
+            # Skip '{'
+            self.i += 1
+
+            res = set()
+
+            while True:
+                part = parse_expr()
+                res |= part
+
+                if self.s[self.i] == ",":
+                    self.i += 1
+                else:
+                    break
+
+            # Skip '}'
+            self.i += 1
+
+            return res
+
+        ans = parse_expr()
+        return sorted(ans)
